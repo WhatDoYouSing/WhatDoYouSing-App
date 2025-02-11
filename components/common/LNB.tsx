14d51dbd91e5be29b523e3ac1b7abc15f66f26a2
youtheyeon <yuseoyeon@ewhain.net>
2025-02-11 17:28:08 +0900

    feat: NativeWind 적용

--- components/common/LNB.tsx
+++ components/common/LNB.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import colors from 'styles/colors';
+import { View, TouchableOpacity } from 'react-native';
+import Typo from './Typo';
 
 interface LNBProps {
   menuItems: string[];
@@ -10,7 +10,7 @@ interface LNBProps {
 
 const LNB = ({ menuItems, selectedItem, onMenuSelect }: LNBProps) => {
   return (
-    <View style={styles.container}>
+    <View className="flex flex-row w-full bg-secondaryBg border-b border-black h-11 box-content">
       {menuItems.map((label, index) => {
         const isSelected = selectedItem === label;
         const isLastItem = index === menuItems.length - 1;
@@ -18,51 +18,20 @@ const LNB = ({ menuItems, selectedItem, onMenuSelect }: LNBProps) => {
         return (
           <TouchableOpacity
             key={label}
-            style={[
-              styles.menuItem,
-              isSelected && styles.selectedMenuItem,
-              isLastItem && styles.lastMenuItem,
-            ]}
+            className={`flex-1 justify-center px-5 ${isSelected ? 'bg-black' : ''} ${isLastItem ? 'border-r-0' : 'border-r border-black'}`}
             onPress={() => onMenuSelect(label)}
           >
-            <Text style={[styles.menuText, isSelected && styles.selectedMenuText]}>
+            <Typo
+              variant={'text-16_M'}
+              className={`${isSelected ? 'text-primaryBg' : 'text-black'}`}
+            >
               {label}
-            </Text>
+            </Typo>
           </TouchableOpacity>
         );
       })}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    backgroundColor: colors.secondaryBg,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.black,
-    height: 60,
-  },
-  menuItem: {
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-    flex: 1,
-    borderRightWidth: 1,
-    borderRightColor: colors.black,
-  },
-  lastMenuItem: {
-    borderRightWidth: 0,
-  },
-  selectedMenuItem: {
-    backgroundColor: colors.black,
-  },
-  menuText: {
-    fontSize: 16,
-    color: colors.black,
-  },
-  selectedMenuText: {
-    color: colors.primaryBg,
-  },
-});
 
 export default LNB;
--- hooks/useTabMenu.ts
+++ hooks/useTabMenu.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 export function useTabMenu(menuItems: string[]) {
-  const [selectedItem, setSelectedItems] = useState<string>(menuItems[0]);
+  const [selectedItem, setSelectedItems] = useState(menuItems[0]);
 
   const handleMenuSelect = (label: string) => {
     setSelectedItems(label);
