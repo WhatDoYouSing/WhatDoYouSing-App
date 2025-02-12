b1ff280cc40c981f82cea643f03c6d667f8bf6b2
youtheyeon <yuseoyeon@ewhain.net>
2025-02-12 22:36:44 +0900

    feat : 하단 메뉴 컴포넌트 구현

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,5 +1,6 @@
+import BottomMenu from './BottomMenu';
 import Header from './Header';
 import LNB from './LNB';
 import Typo from './Typo';
 
-export { Header, LNB, Typo };
+export { BottomMenu, Header, LNB, Typo };
