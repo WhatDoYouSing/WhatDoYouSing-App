7fd906a93bad250c5c5fc26b702dbe689d3a2c3b
JiwonChoi0805 <won0805@ewhain.net>
2025-02-15 03:52:54 +0900

    feat: 하단모달 작업 원복

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,7 @@
-import 'react-native-reanimated';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import BottomNavigator from 'navigation/BottomNavigator';
 
 const RootScreen = () => {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-        <BottomNavigator />
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
-  );
+  return <BottomNavigator />;
 };
 
 export default RootScreen;
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,23 +1,12 @@
-import { Button, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Typo } from 'components/common';
-import { useCallback, useRef } from 'react';
-import { BottomSheetModal } from '@gorhom/bottom-sheet';
-import BottomModal from 'components/common/BottomModal';
 
 const HomeScreen = () => {
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-
-  const handleClosePress = () => bottomSheetModalRef.current?.close();
-  const handleOpenPress = () => bottomSheetModalRef.current?.present();
-
   return (
     <View>
       <Typo variant="text-18_SB" className="text-brand">
         Home
       </Typo>
-      <Button onPress={handleOpenPress} title="open" />
-      <BottomModal ref={bottomSheetModalRef} />
-      <Text>바텀 모달 아래</Text>
     </View>
   );
 };
