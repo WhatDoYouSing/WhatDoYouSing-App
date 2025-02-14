ccdb1a05cdec68b9838f745f050fcf128cf180d6
JiwonChoi0805 <won0805@ewhain.net>
2025-02-15 04:29:58 +0900

    fix: router.d.ts 변경사항 삭제

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,9 +6,41 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string | object = string> {
-      hrefInputParams: { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/../components/common/Tags/Chip`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/screens/HomeScreen`; params?: Router.UnknownInputParams; } | { pathname: `/screens/MyScreen`; params?: Router.UnknownInputParams; } | { pathname: `/screens/RecordScreen`; params?: Router.UnknownInputParams; } | { pathname: `/screens/UploadScreen`; params?: Router.UnknownInputParams; };
-      hrefOutputParams: { pathname: Router.RelativePathString, params?: Router.UnknownOutputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownOutputParams } | { pathname: `/`; params?: Router.UnknownOutputParams; } | { pathname: `/../components/common/Tags/Chip`; params?: Router.UnknownOutputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams; } | { pathname: `/screens/HomeScreen`; params?: Router.UnknownOutputParams; } | { pathname: `/screens/MyScreen`; params?: Router.UnknownOutputParams; } | { pathname: `/screens/RecordScreen`; params?: Router.UnknownOutputParams; } | { pathname: `/screens/UploadScreen`; params?: Router.UnknownOutputParams; };
-      href: Router.RelativePathString | Router.ExternalPathString | `/${`?${string}` | `#${string}` | ''}` | `/../components/common/Tags/Chip${`?${string}` | `#${string}` | ''}` | `/_sitemap${`?${string}` | `#${string}` | ''}` | `/screens/HomeScreen${`?${string}` | `#${string}` | ''}` | `/screens/MyScreen${`?${string}` | `#${string}` | ''}` | `/screens/RecordScreen${`?${string}` | `#${string}` | ''}` | `/screens/UploadScreen${`?${string}` | `#${string}` | ''}` | { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/../components/common/Tags/Chip`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/screens/HomeScreen`; params?: Router.UnknownInputParams; } | { pathname: `/screens/MyScreen`; params?: Router.UnknownInputParams; } | { pathname: `/screens/RecordScreen`; params?: Router.UnknownInputParams; } | { pathname: `/screens/UploadScreen`; params?: Router.UnknownInputParams; };
+      hrefInputParams:
+        | { pathname: Router.RelativePathString; params?: Router.UnknownInputParams }
+        | { pathname: Router.ExternalPathString; params?: Router.UnknownInputParams }
+        | { pathname: `/`; params?: Router.UnknownInputParams }
+        | { pathname: `/_sitemap`; params?: Router.UnknownInputParams }
+        | { pathname: `/screens/HomeScreen`; params?: Router.UnknownInputParams }
+        | { pathname: `/screens/MyScreen`; params?: Router.UnknownInputParams }
+        | { pathname: `/screens/RecordScreen`; params?: Router.UnknownInputParams }
+        | { pathname: `/screens/UploadScreen`; params?: Router.UnknownInputParams };
+      hrefOutputParams:
+        | { pathname: Router.RelativePathString; params?: Router.UnknownOutputParams }
+        | { pathname: Router.ExternalPathString; params?: Router.UnknownOutputParams }
+        | { pathname: `/`; params?: Router.UnknownOutputParams }
+        | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams }
+        | { pathname: `/screens/HomeScreen`; params?: Router.UnknownOutputParams }
+        | { pathname: `/screens/MyScreen`; params?: Router.UnknownOutputParams }
+        | { pathname: `/screens/RecordScreen`; params?: Router.UnknownOutputParams }
+        | { pathname: `/screens/UploadScreen`; params?: Router.UnknownOutputParams };
+      href:
+        | Router.RelativePathString
+        | Router.ExternalPathString
+        | `/${`?${string}` | `#${string}` | ''}`
+        | `/_sitemap${`?${string}` | `#${string}` | ''}`
+        | `/screens/HomeScreen${`?${string}` | `#${string}` | ''}`
+        | `/screens/MyScreen${`?${string}` | `#${string}` | ''}`
+        | `/screens/RecordScreen${`?${string}` | `#${string}` | ''}`
+        | `/screens/UploadScreen${`?${string}` | `#${string}` | ''}`
+        | { pathname: Router.RelativePathString; params?: Router.UnknownInputParams }
+        | { pathname: Router.ExternalPathString; params?: Router.UnknownInputParams }
+        | { pathname: `/`; params?: Router.UnknownInputParams }
+        | { pathname: `/_sitemap`; params?: Router.UnknownInputParams }
+        | { pathname: `/screens/HomeScreen`; params?: Router.UnknownInputParams }
+        | { pathname: `/screens/MyScreen`; params?: Router.UnknownInputParams }
+        | { pathname: `/screens/RecordScreen`; params?: Router.UnknownInputParams }
+        | { pathname: `/screens/UploadScreen`; params?: Router.UnknownInputParams };
     }
   }
 }
