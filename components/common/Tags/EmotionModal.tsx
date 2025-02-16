840828642b9fffd73f5e2d5ae1ebef4771df16c8
jiwonnchoi <won0805@ewhain.net>
2025-02-16 17:07:25 +0900

    feat: 감정모달 사용 배열 변경

--- components/common/Tags/EmotionModal.tsx
+++ components/common/Tags/EmotionModal.tsx
@@ -1,20 +1,17 @@
 import { View } from 'react-native';
 import Tag from './Tag';
-import { MODAL_EMOTIONS } from 'constants/tag';
+import { EMOTION_MAP } from 'constants/emotions';
 
 const EmotionModal = () => {
+  const emotionsArray = Object.values(EMOTION_MAP);
+
   return (
-    <View className="w-[364px] border border-black rounded-[2px] px-[14px] py-[7px] shadow-[0_0_5px_1px_rgba(51,41,44,0.20)]">
-      {MODAL_EMOTIONS.map((row, rowIndex) => (
-        <View
-          key={rowIndex}
-          className="flex flex-row flex-wrap justify-center gap-x-[10px] my-[7px]"
-        >
-          {row.map((emotion, idx) => (
-            <Tag key={idx} text={emotion} size="big" />
-          ))}
-        </View>
-      ))}
+    <View className="w-[358px] border border-black rounded-[2px] py-[7px] shadow-[0_0_5px_1px_rgba(51,41,44,0.20)]">
+      <View className="flex flex-row flex-wrap justify-center gap-x-[10px] gap-y-[7px]">
+        {emotionsArray.map((emotion, idx) => (
+          <Tag key={idx} text={emotion} size="big" />
+        ))}
+      </View>
     </View>
   );
 };
