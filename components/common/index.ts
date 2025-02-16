9d3b16d47f1240ddc37ad2ad5a43251cbb310214
jiwonnchoi <won0805@ewhain.net>
2025-02-16 14:41:10 +0900

    feat: index.ts 임포트 추가

--- components/common/index.ts
+++ components/common/index.ts
@@ -10,6 +10,12 @@ import PlaylistCard from './Card/PlaylistCard';
 import SongInfo from './Card/SongInfo';
 import PlaylistInfo from './Card/PlaylistInfo';
 import ProfileHeader from './Card/ProfileHeader';
+import BaseModal from './BaseModal';
+import Chip from './Tags/Chip';
+import EmotionDetailTags from './Tags/EmotionDetailTags';
+import EmotionModal from './Tags/EmotionModal';
+import FilterTags from './Tags/FilterTags';
+import TagSection from './Tags/TagSection';
 
 export {
   BottomMenu,
@@ -24,4 +30,10 @@ export {
   SongInfo,
   PlaylistInfo,
   ProfileHeader,
+  BaseModal,
+  Chip,
+  EmotionDetailTags,
+  EmotionModal,
+  FilterTags,
+  TagSection,
 };
