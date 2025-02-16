b34e28fb2cae7fde3c5921803df12b94827eae39
jiwonnchoi <won0805@ewhain.net>
2025-02-16 16:09:26 +0900

    feat: IcPlus 변경

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { View } from 'react-native';
-import { FilterTags, Typo } from 'components/common';
+import { Typo } from 'components/common';
 
 const HomeScreen = () => {
   return (
@@ -7,7 +7,6 @@ const HomeScreen = () => {
       <Typo variant="text-18_SB" className="text-brand">
         Home
       </Typo>
-      <FilterTags />
     </View>
   );
 };
--- components/common/Tags/EmotionDetailTags.tsx
+++ components/common/Tags/EmotionDetailTags.tsx
@@ -1,6 +1,7 @@
 import { ScrollView, TouchableOpacity, View } from 'react-native';
 import Tag from './Tag';
 import Typo from '../Typo';
+import { IcPlus } from 'assets/svgs';
 
 interface EmotionData {
   id: number;
@@ -28,8 +29,8 @@ const EmotionDetailTags = ({
       className="py-2 pl-4 pr-3"
     >
       <TouchableOpacity onPress={onPressAddEmotion}>
-        <View className="border border-black rounded-[2px] w-[42px] h-[38.5px] bg-secondaryBg mr-1 flex justify-center items-center">
-          <Typo variant="text-18_M">+</Typo>
+        <View className="border border-black rounded-[2px] px-3 pt-3 pb-[11px] bg-secondaryBg mr-1 flex justify-center items-center">
+          <IcPlus width={14} height={14} />
         </View>
       </TouchableOpacity>
 
