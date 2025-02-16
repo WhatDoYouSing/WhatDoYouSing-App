5cc3b7719aafe9ef7b37e2b167b74511407aebf5
jiwonnchoi <won0805@ewhain.net>
2025-02-16 15:06:56 +0900

    feat: 상수 분리

--- components/common/Tags/EmotionModal.tsx
+++ components/common/Tags/EmotionModal.tsx
@@ -1,16 +1,11 @@
 import { View } from 'react-native';
 import Tag from './Tag';
+import { MODAL_EMOTIONS } from 'constants/Tags';
 
 const EmotionModal = () => {
-  const emotions = [
-    ['행복', '설렘', '희망', '사랑', '평온'],
-    ['벅참', '감동', '쾌감', '신남', '용기'],
-    ['센치함', '그리움', '위로', '슬픔', '비장'],
-  ];
-
   return (
     <View className="w-[364px] border border-black rounded-[2px] px-[14px] py-[7px] shadow-[0_0_5px_1px_rgba(51,41,44,0.20)]">
-      {emotions.map((row, rowIndex) => (
+      {MODAL_EMOTIONS.map((row, rowIndex) => (
         <View
           key={rowIndex}
           className="flex flex-row flex-wrap justify-center gap-x-[10px] my-[7px]"
--- components/common/Tags/FilterTags.tsx
+++ components/common/Tags/FilterTags.tsx
@@ -1,34 +1,12 @@
 import { useState } from 'react';
 import TagSection from './TagSection';
+import { EMOTIONS, TIMES, SEASONS, CONTEXTS, UPLOAD_EMOTIONS } from 'constants/Tags';
 
 interface FilterTagsProps {
   isUpload?: boolean;
   showEmotion?: boolean;
 }
 const FilterTags = ({ isUpload, showEmotion }: FilterTagsProps) => {
-  // 필터 태그 용
-  const emotions = [
-    ['행복', '설렘', '희망', '사랑', '평온', '벅참', '감동'],
-    ['쾌감', '신남', '용기', '센치함', '그리움', '위로'],
-    ['슬픔', '비장'],
-  ];
-  const times = [['새벽', '아침', '오후', '저녁', '밤']];
-  const seasons = [['봄', '여름', '가을', '겨울']];
-  const contexts = [
-    ['산책', '여행', '피크닉', '드라이브', '자기전'],
-    ['출근/등교', '퇴근/하교'],
-    ['일', '공부', '독서', '운동', '집안일', '외출준비'],
-    ['공연', '파티', '새해', '졸업'],
-    ['카페', '집', '회사', '노래방'],
-  ];
-
-  // 감정등록_업로드 용
-  const uploadEmotions = [
-    ['행복', '설렘', '희망', '사랑', '평온'],
-    ['벅참', '감동', '쾌감', '신남', '용기'],
-    ['센치함', '그리움', '위로', '슬픔', '비장'],
-  ];
-
   const [selectedFilter, setSelectedFilter] = useState({
     // 카테고리 별 여러 개의 태그 선택 가능
     감정: new Set<string>(),
@@ -54,7 +32,7 @@ const FilterTags = ({ isUpload, showEmotion }: FilterTagsProps) => {
       {showEmotion && (
         <TagSection
           title={!isUpload && '감정'}
-          tags={isUpload ? uploadEmotions : emotions}
+          tags={isUpload ? UPLOAD_EMOTIONS : EMOTIONS}
           selectedTags={Array.from(selectedFilter['감정'])}
           onSelectTag={(tag) => handleSelectedFilter('감정', tag)}
         />
@@ -63,19 +41,19 @@ const FilterTags = ({ isUpload, showEmotion }: FilterTagsProps) => {
         <>
           <TagSection
             title="시간"
-            tags={times}
+            tags={TIMES}
             selectedTags={Array.from(selectedFilter['시간'])}
             onSelectTag={(tag) => handleSelectedFilter('시간', tag)}
           />
           <TagSection
             title="계절"
-            tags={seasons}
+            tags={SEASONS}
             selectedTags={Array.from(selectedFilter['계절'])}
             onSelectTag={(tag) => handleSelectedFilter('계절', tag)}
           />
           <TagSection
             title="일상맥락"
-            tags={contexts}
+            tags={CONTEXTS}
             selectedTags={Array.from(selectedFilter['일상맥락'])}
             onSelectTag={(tag) => handleSelectedFilter('일상맥락', tag)}
           />
